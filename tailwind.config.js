f9bd31eb9c1982986cb0ca548f2b983c5c20677a
raima32 <raimahimu45@gmail.com>
2025-01-29 23:32:17 +0600

    legel-solition

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // Scan all HTML files in the project root
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
